da0a9da0b04d7e6e39855d1c10809b0e41fdaca1
Chris Schwartz <chris.f.schwartz@gmail.com>
2018-05-07 07:00:14 -0500

    Delete pickled data files

--- script.sh
+++
@@ -1,5 +0,0 @@
-git filter-branch --env-filter '
-    if [ "$GIT_AUTHOR_NAME" != "Chris Schwartz" ]; then \
-        export GIT_AUTHOR_NAME="Chris Schwartz" GIT_AUTHOR_EMAIL="chris.f.schwartz@gmail.com"; \
-    fi
-    '
